c3d672fc455874401a566459475ea5d9afb8b26c
Higorzenaide <higorz.almeida@gmail.com>
2024-01-12 15:23:51 -0300

    Atualização

--- pages/Supervisor COP.py
+++ pages/Supervisor COP.py
@@ -11,22 +11,13 @@ from PIL import Image
 def main():
     definirVariaveisDaSessao()
     configuracoesIniciais()
+    imagemSideBar()
     select = st.sidebar.selectbox('&nbsp;',['Selecione','Cadastrar colaborador','Inserir feedbacks','Visualizar feedbacks','Registrar presença','Inserir advertencia'])
-    #Caminho da imagem
-    imagem_path = "F:\\Feedbacks\\feedbacksDesktop\\images\\logo3.jpeg"
-    imagem = Image.open(imagem_path)
-
-    # Dividir a página em colunas
-    col1, col2, col3 = st.sidebar.columns([1, 1, 1])
-
-    # Coluna 1: Adicionar imagem à barra lateral
-    with col1:
-        st.sidebar.image(imagem, use_column_width=True)
-        st.sidebar.subheader("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; GESTÃO COP")
 
     st.sidebar.button('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
                       '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sair&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
                       '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;')
+    
     if select == 'Inserir feedbacks':
         mainInserirDados()
     elif select == 'Visualizar feedbacks':
