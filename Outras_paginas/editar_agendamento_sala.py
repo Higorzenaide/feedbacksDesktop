54001e8ea5ca8be8da74ac4397e587ff0a32819f
Higorzenaide <higorz.almeida@gmail.com>
2024-01-18 12:34:58 -0300

    Atualização

--- Outras_paginas/editar_agendamento_sala.py
+++ Outras_paginas/editar_agendamento_sala.py
@@ -8,19 +8,18 @@ def main():
     imagem_url = "images/desenvolvimento1.png"  # Substitua pela URL da sua imagem
     st.image(imagem_url, use_column_width=True)
 
-    #Selecionar a data que deseja visualizar
-    event_date = st.date_input("Selecione a data que deseje editar o agendamento:")
-
-    #Formatando a data
-    data_atual = event_date
-    data_formatada = data_atual.strftime("%Y-%m-%d")
-
     instanciarSupa = SupabaseClient()
 
 
     if st.session_state.logado == False:
             efetuarLogin()
             return
+    #Selecionar a data que deseja visualizar
+    event_date = st.date_input("Selecione a data que deseje editar o agendamento:")
+
+    #Formatando a data
+    data_atual = event_date
+    data_formatada = data_atual.strftime("%Y-%m-%d")
     instanciarSupa.editarAgendamento(st.session_state.id,data_formatada)
 
 if __name__ == '__main__':
