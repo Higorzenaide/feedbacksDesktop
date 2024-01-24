e1a192c2d61ab99675e502320086ba9fd23d1b2b
Higorzenaide <higorz.almeida@gmail.com>
2024-01-24 09:33:41 -0300

    atualização

--- Outras_paginas/cadastrar_colaborador.py
+++ Outras_paginas/cadastrar_colaborador.py
@@ -4,12 +4,12 @@ from models.dataBase import SupabaseClient
 from datetime import datetime, timedelta
 from models.funcoes import configuracoesIniciais,imagemSideBar,definirVariaveisDaSessao,efetuarLogin,logout,headerLogin,lerJsonGif,mostrarGif
 
-# Variáveis
-data_minima = datetime(1900, 1, 1)
-data_maxima = datetime.now()
+
 
 def main():
-
+    # Variáveis
+    data_minima = datetime(1900, 1, 1)
+    data_maxima = datetime.now()
     #Se a variavel logado for igual a False, exibe que precisa estar logado
     if st.session_state.logado == False:
         if st.session_state.sessao:
@@ -31,6 +31,7 @@ def main():
         input_cargo = st.selectbox("Insira o cargo do colaborador", ['selecione', 'Auxiliar COP', 'Analista COP'])
         data_nascimento = st.date_input("Insira a data de nascimento do colaborador", min_value=data_minima, max_value=data_maxima)
         numero_telefone = st.text_input(label="Número de telefone pessoal")
+    
         input_button = st.form_submit_button("Cadastrar")
 
 if __name__ == '__main__':
